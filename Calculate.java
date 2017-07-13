b13e6d492d52ce7e4afa5f89be5fb84c2ee29fd2
Ivan Patskun <ivan.patskun@gmail.com>
2017-07-13 18:53:41 +0300

    Calculate Sum

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,9 @@
 public class Calculate {
 	public static void main (String[] arg){
 		System.out.println("Calculate");
+		int first = 4;
+		int second = 5;
+		int sum = first + second;
+		System.out.println ("Sum :" + sum);
 	}
 }
