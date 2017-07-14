a523459b4c57dd38c4ea722364f53a227eb12463
Ivan Patskun <ivan.patskun@gmail.com>
2017-07-14 04:54:34 +0300

    Operation

---
+++ Operation.java
@@ -0,0 +1,16 @@
+public class Operation {
+		public static void main (String[] arg){
+			double first = 5;
+			double second = 7;
+
+			double sum = first + second;
+			double substract = first - second;
+			double multiplication = first * second;
+			double divide = first/second;
+
+			System.out.println("Sum "+ sum);
+			System.out.println("Sub "+ substract);
+			System.out.println("Mul "+ multiplication);
+			System.out.println("Div "+ divide);
+		}
+}
